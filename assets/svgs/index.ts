b1d029f52a3aa91e09a6a7af0e3ef2eaeb8aa710
Frank  Adeleye <oluwatimiadeleye22@gmail.com>
2022-09-20 04:21:27 +0100

    feat: added custom d.ts file

---
+++ assets/svgs/index.ts
@@ -0,0 +1,6 @@
+export { default as LogoSvg } from "./logo.svg";
+import LogoSvg from "a/logo.svg";
+import BlogImageSample from "assets/svgs/blog-image.svg";
+import BlogImageSample2 from "assets/svgs/blog-image-2.svg";
+import BlogAuthorAvatarSample from "assets/svgs/user-avatar-sample.svg";
+import SubscribeBannerBg from "assets/svgs/subscribe.svg";
